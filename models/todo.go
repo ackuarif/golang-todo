708330e38fccc160f98b5379442a287473b30bf0
unknown <ackuarif@gmail.com>
2023-09-29 18:33:36 +0700

    fixing update and omitempty struct

--- models/todo.go
+++ models/todo.go
@@ -2,6 +2,6 @@ package models
 
 type Todo struct {
 	Id int 		`json:"id,omitempty"`
-	Todo string `json:"todo" validate:"required" example:"This is my todo"`
-	Date string `json:"date" validate:"required" example:"2023-01-03"`
+	Todo string `json:"todo,omitempty" validate:"required" example:"This is my todo"`
+	Date string `json:"date,omitempty" validate:"required" example:"2023-01-03"`
 }

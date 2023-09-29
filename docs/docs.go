5016e47df54f3961d9142ba893dd22eeaf0e9cc7
unknown <ackuarif@gmail.com>
2023-09-29 07:38:47 +0700

    update swagger

--- docs/docs.go
+++ docs/docs.go
@@ -330,7 +330,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8000",
+	Host:             "https://todoapp.osc-fr1.scalingo.io",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Swagger Todo Application",
